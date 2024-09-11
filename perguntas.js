4fdcc86b4bbf12ceea449c8764dc8c513ef90ab8
Sol207 <144239715+Sol207@users.noreply.github.com>
2024-09-11 15:11:08 -0300

    projeto cursoooo

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Cursos Disponiveis.',
+    'Vários idiomas.',
+    'Presencial e online.'
+)
+
+criaCartao(
+    'ESPANHOL',
+    'Aprender espanhol é uma oportunidade incrível que vai muito além de apenas dominar uma nova língua. Com mais de 580 milhões de falantes no mundo, o espanhol abre portas para novas culturas, amizades e oportunidades profissionais. Ao se comunicar em espanhol, você poderá viajar por países da América Latina e Espanha com mais facilidade, entender músicas e filmes em sua essência e até mesmo ampliar suas perspectivas de carreira em um mercado cada vez mais globalizado. Além disso, o aprendizado de uma nova língua estimula o cérebro, melhora a memória e a concentração.',
+    'A partir de 350,99 por mês.'
+)
+
+criaCartao(
+    'FRANCÊS',
+    'Fazer um curso de francês é uma excelente escolha para quem deseja expandir seus horizontes e mergulhar em uma cultura rica e diversificada. O francês é a língua da arte, da moda e da gastronomia, e aprender a língua vai além de apenas falar; é uma porta de entrada para entender a literatura, o cinema e a música que moldaram o mundo. Além disso, o francês é falado em vários continentes, tornando-se uma habilidade valiosa no mercado de trabalho globalizado. Ao dominar essa língua encantadora, você poderá viajar pela França e outros países francófonos com confiança, fazer novas amizades e ter acesso a experiências únicas.?',
+    '269,90 por mês.'
+)
+
+criaCartao(
+    'ÁRABE',
+    'Fazer um curso de árabe é uma experiência transformadora que te conecta a uma das culturas mais ricas e fascinantes do mundo. Com mais de 400 milhões de falantes, o árabe é a língua oficial de diversos países e um dos idiomas mais influentes em áreas como negócios, diplomacia e estudos religiosos. Aprender árabe não só enriquece sua compreensão de histórias, tradições e filosofias milenares, mas também abre portas para oportunidades profissionais em mercados emergentes. Além disso, dominar o árabe te permite interagir com pessoas de diferentes origens, ampliando sua rede de contatos e suas perspectivas globais.',
+    '566,80 por mês.'
+)
